a7396c44626df7bf9bfb370fa69aa933785baffa
Fathima N <fathima97570@gmail.com>
2026-07-28 23:57:09 +0530

    Time: 389 ms (64.71%) | Memory: 15.4 MB (14.71%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def smallestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s) == 1:
+            return s
+
+        n = len(s)
+        half = n // 2
+
+        # Sort the first half
+        left = sorted(s[:half])
+
+        if n % 2 == 0:
+            return "".join(left + left[::-1])
+        else:
+            return "".join(left + [s[half]] + left[::-1])
+        
+        

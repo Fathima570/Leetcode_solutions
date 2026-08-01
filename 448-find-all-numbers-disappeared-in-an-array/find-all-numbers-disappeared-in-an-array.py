0a9007b8f1c98facff5fd7778a35f70f8d86199f
Fathima N <fathima97570@gmail.com>
2026-08-01 23:43:32 +0530

    Time: 26 ms (92.00%) | Memory: 26.7 MB (72.03%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -4,13 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        s = set(nums)
-        ans = []
-
+        n=len(nums)
+        s=set(nums)
+        ans=[]
         for i in range(1, n + 1):
             if i not in s:
                 ans.append(i)
-
         return ans
 
